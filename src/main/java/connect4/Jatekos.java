b4f8a999f0a5c85ba93d07fb454e4d32bf44a703
kkristof02 <kkristof1124@gmail.com>
2024-11-19 15:22:54 +0100

    comments added

--- src/main/java/connect4/Jatekos.java
+++ src/main/java/connect4/Jatekos.java
@@ -1,31 +1,40 @@
 package connect4;
 
+// A Jatekos osztály a játékosokat reprezentálja, nevüket és győzelmeik számát tárolja
 public class Jatekos {
+    // A játékos nevét tároló mező
     private final String nev;
+    // A játékos győzelmeinek számát tároló mező
     private int gyozelmek;
 
+    // Konstruktor, amely beállítja a játékos nevét és győzelmeinek számát
     public Jatekos(String nev, int gyozelmek) {
         this.nev = nev;
         this.gyozelmek = gyozelmek;
     }
 
+    // Getter metódus a játékos nevének lekérdezéséhez
     public String getNev() {
         return nev;
     }
 
+    // Getter metódus a játékos győzelmeinek számának lekérdezéséhez
     public int getGyozelmek() {
         return gyozelmek;
     }
 
+    // Metódus a játékos győzelmeinek számának növeléséhez
     public void novelGyozelmek() {
         this.gyozelmek++;
     }
 
+    // A játékos adatait string formátumban visszaadó metódus
     @Override
     public String toString() {
         return nev + ":" + gyozelmek;
     }
 
+    // Statikus metódus, amely egy string alapján létrehoz egy Jatekos objektumot
     public static Jatekos fromString(String str) {
         String[] parts = str.split(":");
         return new Jatekos(parts[0], Integer.parseInt(parts[1]));
--- src/main/java/connect4/Korong.java
+++ src/main/java/connect4/Korong.java
@@ -1,14 +1,19 @@
 package connect4;
 
+// A Korong osztály a játékban használt korongokat reprezentálja
 public class Korong {
+    // A korong színét tároló mező
     private final char szin;
 
+    // Konstruktor, amely beállítja a korong színét
     public Korong(char szin) {
         this.szin = szin;
     }
 
+    // Getter metódus a korong színének lekérdezéséhez
     public char getSzin() {
         return szin;
     }
 }
+
 
