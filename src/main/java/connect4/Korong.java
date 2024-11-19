184a7fafc2b0b2b3666e51401e8a4cfb03c63903
kkristof02 <kkristof1124@gmail.com>
2024-11-19 14:41:02 +0100

    korong v1 ready

---
+++ src/main/java/connect4/Korong.java
@@ -0,0 +1,14 @@
+package connect4;
+
+public class Korong {
+    private final char szin;
+
+    public Korong(char szin) {
+        this.szin = szin;
+    }
+
+    public char getSzin() {
+        return szin;
+    }
+}
+
